4f7982da23b3d28bae53d320ab513a75c3494255
James Lucas <james.lucas.m14@gmail.com>
2025-07-09 17:11:37 -0500

    Pórtafolio

--- public/assets/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector(".menu-toggle");
-
-const menu = document.querySelector("nav ul");
-
-btn.addEventListener("click", () => {
-    menu.classList.toggle("active");
-});
